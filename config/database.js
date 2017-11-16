bdcfe8a888addc081b1566366ae665ddc012373c
Tsogodar <marcin.gryczon@o2.pl>
2017-11-16 11:11:49 +0100

    Poprawka warunku środowiska w konfiguracji bazy

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 module.exports = {
     dbConnect: ()=>{
-        if(process.env.NODE_ENV!=='production'){
+        if(process.env.NODE_ENV==='production'){
             mongoose.connect('mongodb://marcin:dyplo@ds111066.mlab.com:11066/dyplo-prod', {
                 useMongoClient: true
             }).then(() => {

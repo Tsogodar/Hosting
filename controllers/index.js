4fd1d5ce5cd1d8cfe4a510fe70f251cd0f88b106
Tsogodar <marcin.gryczon@o2.pl>
2018-01-08 19:25:47 +0100

    Zmiana modali szczegółów, wiele pomniejszych poprawek

--- controllers/index.js
+++ controllers/index.js
@@ -4,13 +4,15 @@ const folderModel = require('../models/Folder');
 const fileModel = require('../models/File');
 const {authenticated} = require('../helpers/authenticated');
 
-router.get('/',authenticated, (req, res) => {
+router.get('/', authenticated, (req, res) => {
     folderModel.findFolders(req.user.email, null, (folders) => {
         fileModel.findFiles(req.user.email, null, (error, files) => {
+            let countFiles = files.length;
             for (key in files) {
+                files[key].count=countFiles;
                 if (files[key].filename.length > 19) {
                     files[key].filenameShort = `${files[key].filename.substring(0, 15)}...`;
-                } else{
+                } else {
                     files[key].filenameShort = files[key].filename;
                 }
             }
@@ -20,7 +22,7 @@ router.get('/',authenticated, (req, res) => {
                 }
             }
             res.render('main/show', {
-                config:req.user.config,
+                config: req.user.config,
                 folders: folders,
                 files: files
             })
--- public/js/helpers/arrows.js
+++ public/js/helpers/arrows.js
@@ -1,3 +1,48 @@
 $(document).ready(function () {
+    $('.arrRight').on('click', function () {
+        if ($('audio').length != 0) {
+            $('audio')[0].pause();
+        }
+        if ($('video').length != 0) {
+            $('video')[1].pause();
+        }
+        let openedModalId = this.offsetParent.id;
+        let openedModal = $('#' + openedModalId + '.detailsModal');
+        if (openedModal.nextAll('.detailsModal:first')[0] == undefined) {
+            $('#' + openedModalId).modal('close');
+        } else {
+            let nextModalId = openedModal.nextAll('.detailsModal:first')[0].id;
+            $('#' + openedModalId).modal('close');
+            $('#' + nextModalId).modal('open');
+
+        }
+    });
+
+    $('.arrLeft').on('click', function () {
+        if ($('audio').length != 0) {
+            $('audio')[0].pause();
+        }
+        if ($('video').length != 0) {
+            $('video')[1].pause();
+        }
+        let openedModalId = this.offsetParent.id;
+        let openedModal = $('#' + openedModalId + '.detailsModal');
+        if (openedModal.prevAll('.detailsModal:first')[0] == undefined) {
+            $('#' + openedModalId).modal('close');
+        } else {
+            let prevModalId = openedModal.prevAll('.detailsModal:first')[0].id;
+            $('#' + openedModalId).modal('close');
+            $('#' + prevModalId).modal('open');
+        }
+    });
+
+    $('.closeIcon').on('click', function () {
+        if ($('audio').length != 0) {
+            $('audio')[0].pause();
+        }
+        if ($('video').length != 0) {
+            $('video')[1].pause();
+        }
+    });
 
 });
